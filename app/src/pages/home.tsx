c097f2dab52f1dc0872f6c9a772d91788bc67d77
Simon <simon820225@gmail.com>
2022-12-25 22:54:56 +0800

    init setup

--- app/src/pages/home.tsx
+++ app/src/pages/home.tsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import axios from "axios";
+
+const SERVER_URL = "http://127.0.0.1:8080";
+
 export const Home = () => {
+  const [url, setUrl] = useState("");
   return (
-    <div style={{ display: "flex", gap: "12px" }}>
-      <input />
-      <button>Start Scraping</button>
+    <div style={{ display: "flex", gap: "12px", flexDirection: "column" }}>
+      <input
+        value={url}
+        placeholder="輸入網址"
+        style={{ width: "500px" }}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <div>
+        <button
+          onClick={() => {
+            setUrl("");
+            axios
+              .get(`${SERVER_URL}/ping`)
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          }}
+        >
+          建立爬蟲任務
+        </button>
+      </div>
     </div>
   );
 };
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello web")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.POST("/scraping", func(c *gin.Context) {
+		message := c.PostForm("message")
+		nick := c.DefaultPostForm("nick", "anonymous")
+
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "posted",
+			"message": message,
+			"nick":    nick,
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
