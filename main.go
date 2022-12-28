60f08601160d5682e074a1fb583979e3830c1b0b
Simon <simon820225@gmail.com>
2022-12-28 21:59:53 +0800

    refactor: post request

--- main.go
+++ main.go
@@ -1,53 +1,47 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
 type Request struct {
 	URL string `json:"url" binding:"required"`
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// router := gin.Default()
+	// router.GET("/ping", func(c *gin.Context) {
+	// 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
 
-	router.POST("/crawl", func(c *gin.Context) {
+	// router.POST("/crawl", func(c *gin.Context) {
 
-		var request Request
+	// 	var request Request
 
-		if err := c.ShouldBindJSON(&request); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "ERROR",
-			})
-			return
-		}
+	// 	if err := c.ShouldBindJSON(&request); err != nil {
+	// 		c.JSON(http.StatusBadRequest, gin.H{
+	// 			"message": "ERROR",
+	// 		})
+	// 		return
+	// 	}
 
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	// 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	// 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	// 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
-		url := request.URL
+	// 	url := request.URL
 
-		c.JSON(http.StatusOK, gin.H{
-			"status": "posted",
-			"url":    url,
-		})
-	})
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"status": "posted",
+	// 		"url":    url,
+	// 	})
+	// })
 
-	router.OPTIONS("/crawl", func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		c.AbortWithStatus(http.StatusOK)
-	})
+	// router.OPTIONS("/crawl", func(c *gin.Context) {
+	// 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	// 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	// 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	// 	c.AbortWithStatus(http.StatusOK)
+	// })
 
-	router.Run() // listen and serve on 0.0.0.0:8080
+	// router.Run() // listen and serve on 0.0.0.0:8080
 }
--- pkg/handler/crawl.go
+++ pkg/handler/crawl.go
@@ -1,6 +1,15 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Request struct {
+	URL string `json:"url" binding:"required"`
+}
 
 // producer
 
@@ -9,8 +18,6 @@ func Crawl(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
-	c.JSON(200, "Hellov1")
 
 	// method 1 一條龍 ⭐️
 	// TODO extract request body form post context
@@ -21,8 +28,25 @@ func Crawl(c *gin.Context) {
 	// method 2 ⭐️⭐️⭐️⭐️⭐️️️
 	// 思考，是否在這裡預處理
 	// TODO extract request body form post context
+	var request Request
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "ERROR",
+		})
+		return
+	}
+
+	url := request.URL
+	fmt.Println("網址", url)
 	// TODO enqueue pkg/taskQueue (body)
 	// TODO error handle => response reject
 	// TODO response accept
+
+	// c.JSON(200, "Hellov1")
+	c.JSON(http.StatusOK, gin.H{
+		"status": "OK",
+		"url":    url,
+	})
 
 }
