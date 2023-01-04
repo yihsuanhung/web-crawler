31ef86b955c0cb8d517db8b525f8126460410e9b
yihsuanhung <simon820225@gmail.com>
2023-01-04 22:56:01 +0800

    refactor: update component structure

--- web/app/src/pages/home.tsx
+++ web/app/src/pages/home.tsx
@@ -1,44 +1,11 @@
-import React, { useState } from "react";
-import axios from "axios";
-
-const SERVER_URL = "http://127.0.0.1:8090/v1";
-
-const postReq = async (url: string) => {
-  const response = await axios.post(`${SERVER_URL}/crawl`, {
-    url,
-  });
-  console.log("url", url, response);
-};
-
-const getReq = async () => {
-  const response = await axios.get(`${SERVER_URL}/hello`);
-  console.log(response);
-};
+import React from "react";
+import { Detail, Task } from "../components";
 
 export const Home = () => {
-  const [url, setUrl] = useState("https://developer.mozilla.org/zh-TW/");
   return (
-    <div style={{ display: "flex", gap: "12px", flexDirection: "column" }}>
-      <input
-        value={url}
-        placeholder="輸入網址"
-        style={{ width: "500px" }}
-        onChange={(e) => setUrl(e.target.value)}
-      />
-      <div>
-        <button
-          style={{ margin: "6px" }}
-          onClick={() => {
-            postReq(url);
-            // getReq();
-          }}
-        >
-          建立爬蟲任務
-        </button>
-        <button style={{ margin: "6px" }} onClick={() => setUrl("")}>
-          重置網址
-        </button>
-      </div>
+    <div style={{ display: "flex", flexDirection: "column", gap: "60px" }}>
+      <Task />
+      <Detail />
     </div>
   );
 };
